a620fb75f0e89451155d75541017533758a39ac5
Nguyễn Thành Nam <namnguyenthanh.work@gmail.com>
2024-01-06 15:16:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -87,7 +87,7 @@ window.addEventListener('load', () => {
     let totalSeconds;
 
     function init() {
-        totalSeconds = Math.floor((new Date('28/06/2024') - new Date()) / 1000);
+        totalSeconds = Math.floor((new Date("2024-06-28") - new Date()) / 1000);
         setTimeLeft();
         let interval = setInterval(() => {
             if (totalSeconds < 0) {
